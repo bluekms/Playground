b2a8db2cf1c40f210d585ad2473aa92830adfb42
minseok.kang <bluekms21@naver.com>
2022-02-02 16:23:13 +0900

    .editorconfig 적용 및 모든 워닝 제거

--- Servers/WorldServer/Services/GreeterService.cs
+++ Servers/WorldServer/Services/GreeterService.cs
@@ -8,6 +8,7 @@ namespace WorldServer.Services
     public class GreeterService : Greeter.GreeterBase
     {
         private readonly ILogger<GreeterService> _logger;
+
         public GreeterService(ILogger<GreeterService> logger)
         {
             _logger = logger;
@@ -17,7 +18,7 @@ namespace WorldServer.Services
         {
             return Task.FromResult(new HelloReply
             {
-                Message = "Hello " + request.Name
+                Message = "Hello " + request.Name,
             });
         }
     }
