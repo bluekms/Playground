6d12aeefc73b5e20218b0e4550534bd1428c016b
minseok.kang <bluekms21@naver.com>
2022-02-05 03:06:10 +0900

    AuthContext 추가

---
+++ Servers/AccountServer/Models/AuthContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace AccountServer.Models
+{
+    public class AuthContext : DbContext
+    {
+        public AuthContext(DbContextOptions<AuthContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Account> Accounts { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Account>().HasKey(k => new { k.AccountId });
+        }
+    }
+    // TODO Library로 빼야 하나?
+    public sealed record Account(string AccountId, string Password, string SessionId, DateTime CreatedAt, string Authority);
+}
