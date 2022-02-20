7d17efcc9c56523ceb744d03c389b98a01686e66
minseok.kang <bluekms21@naver.com>
2022-02-20 22:22:23 +0900

    인가 처리

---
+++ Servers/AccountServer/Extensions/Authorizations/ClientRoleHandler.cs
@@ -0,0 +1,59 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using AccountServer.Handlers.Maintenance;
+using CommonLibrary.Handlers;
+using Microsoft.AspNetCore.Authorization;
+
+namespace AccountServer.Extensions.Authorizations
+{
+    public class ClientRoleHandler : AuthorizationHandler<ClientRoleRequirment>
+    {
+        private readonly IQueryHandler<IsMaintenanceTimeQuery, bool> isMaintenanceTime;
+
+        public ClientRoleHandler(IQueryHandler<IsMaintenanceTimeQuery, bool> isMaintenanceTime)
+        {
+            this.isMaintenanceTime = isMaintenanceTime;
+        }
+
+        protected override async Task HandleRequirementAsync(AuthorizationHandlerContext context, ClientRoleRequirment requirement)
+        {
+            if (context.User?.Identity == null)
+            {
+                context.Fail();
+                return;
+            }
+
+            if (!context.User.Identity.IsAuthenticated)
+            {
+                context.Fail();
+                return;
+            }
+
+            var claim = context.User.Claims
+                .FirstOrDefault(x => x.Type == ClientRoleRequirment.ClaimType);
+
+            if (claim == null)
+            {
+                context.Fail();
+                return;
+            }
+
+            if (requirement.ClientRoleList.All(x => x != claim.Value))
+            {
+                context.Fail();
+                return;
+            }
+
+            if (claim.Value == ClientRoleRequirment.ClientRoles.User.ToString())
+            {
+                if (await isMaintenanceTime.QueryAsync(new()))
+                {
+                    context.Fail();
+                    return;
+                }
+            }
+
+            context.Succeed(requirement);
+        }
+    }
+}
