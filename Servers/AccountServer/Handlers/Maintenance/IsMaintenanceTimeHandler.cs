254dd779bf386636d8e5c7d31a730b9eaae458c9
minseok.kang <bluekms21@naver.com>
2022-02-20 19:00:09 +0900

    AddMaintenance

---
+++ Servers/AccountServer/Handlers/Maintenance/IsMaintenanceTimeHandler.cs
@@ -0,0 +1,31 @@
+using System.Linq;
+using System.Threading.Tasks;
+using AuthDb;
+using CommonLibrary;
+using CommonLibrary.Handlers;
+using Microsoft.EntityFrameworkCore;
+
+namespace AccountServer.Handlers.Maintenance
+{
+    public sealed record IsMaintenanceTimeQuery() : IQuery;
+
+    public class IsMaintenanceTimeHandler : IQueryHandler<IsMaintenanceTimeQuery, bool>
+    {
+        private readonly ITimeService timeService;
+        private readonly AuthContext authContext;
+
+        public IsMaintenanceTimeHandler(ITimeService timeService, AuthContext authContext)
+        {
+            this.timeService = timeService;
+            this.authContext = authContext;
+        }
+
+        public async Task<bool> QueryAsync(IsMaintenanceTimeQuery query)
+        {
+            return await authContext.Maintenance
+                .Where(x => x.Start <= timeService.Now)
+                .Where(x => timeService.Now <= x.End)
+                .AnyAsync();
+        }
+    }
+}
