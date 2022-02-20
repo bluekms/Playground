a7fec686053a3be8b0987e2d96d53eadbe5e02bb
minseok.kang <bluekms21@naver.com>
2022-02-20 22:23:20 +0900

    FooController 추가

---
+++ Servers/AccountServer/Controllers/FooController.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Threading.Tasks;
+using AccountServer.Extensions.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace AccountServer.Controllers
+{
+    [ApiController]
+    public class FooController : ControllerBase
+    {
+        private readonly ILogger<FooController> logger;
+
+        public FooController(ILogger<FooController> logger)
+        {
+            this.logger = logger;
+        }
+
+        [HttpPost, Route("Account/Foo")]
+        [Authorize(AuthenticationSchemes = SessionIdAuthenticationSchemeOptions.Name, Policy = "ServiceApi")]
+        public async Task<ActionResult<string>> Foo([FromBody] ArgumentData args)
+        {
+            try
+            {
+                return await HandleAsync(args);
+            }
+            catch (Exception e)
+            {
+                logger.LogError($"{e.Message}:{e.InnerException?.Message ?? string.Empty}");
+                return NotFound();
+            }
+        }
+
+        private async Task<string> HandleAsync(ArgumentData args)
+        {
+            return await Task.Run(() => args.Data + ": OK");
+        }
+
+        public sealed record ArgumentData(string Data);
+    }
+}
