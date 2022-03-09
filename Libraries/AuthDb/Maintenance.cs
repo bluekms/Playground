5e890cd94f1a3c501f2feb5f7989c329aeb34c72
minseok.kang <bluekms21@naver.com>
2022-03-09 23:49:15 +0900

    AuthDb에 HasKey를 별도 파일로 분리

--- Libraries/AuthDb/Maintenance.cs
+++ Libraries/AuthDb/Maintenance.cs
@@ -2,25 +2,34 @@ using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Diagnostics.CodeAnalysis;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-namespace AuthDb
+namespace AuthDb;
+
+public class Maintenance
 {
-    public class Maintenance
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    [Key]
+    public long Id { get; set; }
+
+    public DateTime Start { get; init; }
+
+    public DateTime End { get; init; }
+
+    [AllowNull]
+    public string Reason { get; init; }
+
+    public override string ToString()
     {
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        [Key]
-        public long Id { get; set; }
+        return $"Id: {Id}, Start: {Start}, End: {End}, Reason: {Reason}";
+    }
+}
 
-        public DateTime Start { get; init; }
-
-        public DateTime End { get; init; }
-
-        [AllowNull]
-        public string Reason { get; init; }
-
-        public override string ToString()
-        {
-            return $"Id: {Id}, Start: {Start}, End: {End}, Reason: {Reason}";
-        }
+internal sealed class MaintenanceConfiguration : IEntityTypeConfiguration<Maintenance>
+{
+    public void Configure(EntityTypeBuilder<Maintenance> builder)
+    {
+        builder.HasKey(k => new {k.Id});
     }
 }
