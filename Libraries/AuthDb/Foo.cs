ce766dec4d0585e45ceda8fb3848ec3aef6a7103
minseok.kang <bluekms21@naver.com>
2022-03-09 23:49:15 +0900

    AuthDb에 HasKey를 별도 파일로 분리

--- Libraries/AuthDb/Foo.cs
+++ Libraries/AuthDb/Foo.cs
@@ -1,29 +1,38 @@
 using System.ComponentModel.DataAnnotations;
 using System.Diagnostics.CodeAnalysis;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-namespace AuthDb
+namespace AuthDb;
+
+public sealed class Foo
 {
-    public sealed class Foo
+    [Key]
+    public long Seq { get; set; }
+
+    [AllowNull]
+    public string AccountId { get; set; }
+
+    [AllowNull]
+    public string Command { get; set; }
+
+    public int Value { get; set; }
+
+    public enum FooCommand
     {
-        [Key]
-        public long Seq { get; set; }
+        Addition,
+        Subtraction,
+        Multiplication,
+        Division,
+        Squared,
+        Merge,
+    }
+}
 
-        [AllowNull]
-        public string AccountId { get; set; }
-
-        [AllowNull]
-        public string Command { get; set; }
-
-        public int Value { get; set; }
-
-        public enum FooCommand
-        {
-            Addition,
-            Subtraction,
-            Multiplication,
-            Division,
-            Squared,
-            Merge,
-        }
+internal sealed class FooConfiguration : IEntityTypeConfiguration<Foo>
+{
+    public void Configure(EntityTypeBuilder<Foo> builder)
+    {
+        builder.HasKey(k => new {k.Seq});
     }
 }
