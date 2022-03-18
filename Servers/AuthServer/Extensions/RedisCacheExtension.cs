afc8424c143117838265760044d07bd9382bd7aa
minseok.kang <bluekms21@naver.com>
2022-03-19 01:25:06 +0900

    mysql 버전 수정, connection string에 nullable 적용

--- Servers/AuthServer/Extensions/RedisCacheExtension.cs
+++ Servers/AuthServer/Extensions/RedisCacheExtension.cs
@@ -5,7 +5,7 @@ namespace AuthServer.Extensions
 {
     public static class RedisCacheExtension
     {
-        public static void UseRedisCache(this IServiceCollection services, string connectionString)
+        public static void UseRedisCache(this IServiceCollection services, string? connectionString)
         {
             var redisConnection = ConnectionMultiplexer.Connect(connectionString);
 
