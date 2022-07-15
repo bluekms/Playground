2764dd961f251f791abd161c0d3d54eb8144560c
강민석 <bluekms@shiftup.co.kr>
2022-07-15 20:28:04 +0900

    How to get GetConnectionString with evn
    $env:ASPNETCORE_ENVIRONMENT='Development'

--- Libraries/AuthDb/AuthDbContextFactory.cs
+++ Libraries/AuthDb/AuthDbContextFactory.cs
@@ -1,19 +1,39 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
 using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
 
 namespace AuthDb;
 
 public class AuthDbContextFactory : IDesignTimeDbContextFactory<AuthDbContext>
 {
-    private const string connectionString = "Server=myServerAddress;Database=myDataBase;Uid=myUsername;Pwd=myPassword;";
-    
+    private IConfigurationRoot config;
+
+    public AuthDbContextFactory()
+    {
+        var path = Path.Join(AppContext.BaseDirectory, @"..\..\..\..\..\", @"Servers\AuthServer");
+        string env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(path)
+            .AddJsonFile($"appsettings.{env}.json", optional: false, reloadOnChange: true);
+
+        config = builder.Build();
+    }
+
     public AuthDbContext CreateDbContext(string[] args)
     {
+        var conn = config.GetConnectionString("AuthDb");
+        
         var optionsBuilder = new DbContextOptionsBuilder<AuthDbContext>();
         optionsBuilder.UseMySql(
-            connectionString,
-            ServerVersion.Create(8, 0, 0, ServerType.MySql));
+            conn,
+            ServerVersion.Create(8, 0, 0, ServerType.MySql),
+            builder =>
+            {
+                builder.EnableRetryOnFailure();
+                builder.CommandTimeout(5);
+            });
 
         return new AuthDbContext(optionsBuilder.Options);
     }
