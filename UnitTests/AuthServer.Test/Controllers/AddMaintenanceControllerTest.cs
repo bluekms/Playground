b85215b2c1a7a278b1d670346bbf5d92a9717ccf
강민석 <bluekms@shiftup.co.kr>
2022-07-21 18:11:49 +0900

    namespace 형식 최신화

--- UnitTests/AuthServer.Test/Controllers/AddMaintenanceControllerTest.cs
+++ UnitTests/AuthServer.Test/Controllers/AddMaintenanceControllerTest.cs
@@ -10,53 +10,52 @@ using Shouldly;
 using StackExchange.Redis;
 using Xunit;
 
-namespace AuthServer.Test.Controllers
+namespace AuthServer.Test.Controllers;
+
+public sealed class AddMaintenanceControllerTest : IDisposable
 {
-    public sealed class AddMaintenanceControllerTest : IDisposable
+    private readonly AuthDbFixture authDbFixture;
+    private readonly AuthDbContext dbContext;
+    private readonly IMapper mapper;
+    private readonly ConnectionMultiplexer redisConnection;
+
+    public AddMaintenanceControllerTest()
     {
-        private readonly AuthDbFixture authDbFixture;
-        private readonly AuthDbContext dbContext;
-        private readonly IMapper mapper;
-        private readonly ConnectionMultiplexer redisConnection;
+        authDbFixture = new();
+        dbContext = authDbFixture.CreateContext();
 
-        public AddMaintenanceControllerTest()
-        {
-            authDbFixture = new();
-            dbContext = authDbFixture.CreateContext();
+        var config = InitConfig.Use();
+        redisConnection = ConnectionMultiplexer.Connect(config.GetConnectionString("RedisCache"));
 
-            var config = InitConfig.Use();
-            redisConnection = ConnectionMultiplexer.Connect(config.GetConnectionString("RedisCache"));
+        mapper = InitMapper.Use();
+    }
 
-            mapper = InitMapper.Use();
-        }
+    public void Dispose()
+    {
+        authDbFixture.Dispose();
+        dbContext.Dispose();
+        redisConnection.Dispose();
+    }
 
-        public void Dispose()
-        {
-            authDbFixture.Dispose();
-            dbContext.Dispose();
-            redisConnection.Dispose();
-        }
+    [Theory]
+    [InlineData("2022-03-09 13:00", "2022-03-09 14:00", "Unit Testing")]
+    public async void AddMaintenance(DateTime start, DateTime end, string reason)
+    {
+        var controller = new AddMaintenanceController(
+            mapper,
+            new CheckMaintenanceRule(dbContext),
+            new AddMaintenanceHandler(dbContext, mapper));
 
-        [Theory]
-        [InlineData("2022-03-09 13:00", "2022-03-09 14:00", "Unit Testing")]
-        public async void AddMaintenance(DateTime start, DateTime end, string reason)
-        {
-            var controller = new AddMaintenanceController(
-                mapper,
-                new CheckMaintenanceRule(dbContext),
-                new AddMaintenanceHandler(dbContext, mapper));
+        var result = await controller.AddMaintenance(new(start, end, reason));
+        var actionResult = Assert.IsType<ActionResult<AddMaintenanceController.Returns>>(result);
+        actionResult.Value?.Id.ShouldBe(1);
 
-            var result = await controller.AddMaintenance(new(start, end, reason));
-            var actionResult = Assert.IsType<ActionResult<AddMaintenanceController.Returns>>(result);
-            actionResult.Value?.Id.ShouldBe(1);
+        result = await controller.AddMaintenance(new(start.AddDays(1), end.AddDays(1), reason));
+        actionResult = Assert.IsType<ActionResult<AddMaintenanceController.Returns>>(result);
+        actionResult.Value?.Id.ShouldBe(2);
 
-            result = await controller.AddMaintenance(new(start.AddDays(1), end.AddDays(1), reason));
-            actionResult = Assert.IsType<ActionResult<AddMaintenanceController.Returns>>(result);
-            actionResult.Value?.Id.ShouldBe(2);
-
-            result = await controller.AddMaintenance(new(start.AddDays(3), end.AddDays(3), reason));
-            actionResult = Assert.IsType<ActionResult<AddMaintenanceController.Returns>>(result);
-            actionResult.Value?.Id.ShouldBe(3);
-        }
+        result = await controller.AddMaintenance(new(start.AddDays(3), end.AddDays(3), reason));
+        actionResult = Assert.IsType<ActionResult<AddMaintenanceController.Returns>>(result);
+        actionResult.Value?.Id.ShouldBe(3);
     }
 }
--- UnitTests/AuthServer.Test/Controllers/RegisterServerControllerTest.cs
+++ UnitTests/AuthServer.Test/Controllers/RegisterServerControllerTest.cs
@@ -11,46 +11,45 @@ using Microsoft.Extensions.Configuration;
 using StackExchange.Redis;
 using Xunit;
 
-namespace AuthServer.Test.Controllers
+namespace AuthServer.Test.Controllers;
+
+public sealed class RegisterServerControllerTest : IDisposable
 {
-    public sealed class RegisterServerControllerTest : IDisposable
+    private readonly AuthDbFixture authDbFixture;
+    private readonly AuthDbContext dbContext;
+    private readonly ITimeService timeService;
+    private readonly IMapper mapper;
+    private readonly ConnectionMultiplexer redisConnection;
+
+    public RegisterServerControllerTest()
     {
-        private readonly AuthDbFixture authDbFixture;
-        private readonly AuthDbContext dbContext;
-        private readonly ITimeService timeService;
-        private readonly IMapper mapper;
-        private readonly ConnectionMultiplexer redisConnection;
+        authDbFixture = new();
+        dbContext = authDbFixture.CreateContext();
 
-        public RegisterServerControllerTest()
-        {
-            authDbFixture = new();
-            dbContext = authDbFixture.CreateContext();
+        var config = InitConfig.Use();
+        redisConnection = ConnectionMultiplexer.Connect(config.GetConnectionString("RedisCache"));
 
-            var config = InitConfig.Use();
-            redisConnection = ConnectionMultiplexer.Connect(config.GetConnectionString("RedisCache"));
+        timeService = new ScopedTimeService();
+        mapper = InitMapper.Use();
+    }
 
-            timeService = new ScopedTimeService();
-            mapper = InitMapper.Use();
-        }
+    public void Dispose()
+    {
+        authDbFixture.Dispose();
+        dbContext.Dispose();
+        redisConnection.Dispose();
+    }
 
-        public void Dispose()
-        {
-            authDbFixture.Dispose();
-            dbContext.Dispose();
-            redisConnection.Dispose();
-        }
+    [Theory]
+    [InlineData("KmsWorld", ServerRoles.World, "localhost:1234", "xUnit Test", 60 * 5)]
+    public async void AddServer(string name, ServerRoles role, string address, string description, long expireSec)
+    {
+        var controller = new RegisterServerController(
+            mapper,
+            new UpsertServerHandler(dbContext, timeService, mapper));
 
-        [Theory]
-        [InlineData("KmsWorld", ServerRoles.World, "localhost:1234", "xUnit Test", 60 * 5)]
-        public async void AddServer(string name, ServerRoles role, string address, string description, long expireSec)
-        {
-            var controller = new RegisterServerController(
-                mapper,
-                new UpsertServerHandler(dbContext, timeService, mapper));
+        var result = await controller.RegisterServer(new(name, role, address, description, expireSec));
 
-            var result = await controller.RegisterServer(new(name, role, address, description, expireSec));
-
-            Assert.IsType<OkResult>(result);
-        }
+        Assert.IsType<OkResult>(result);
     }
 }
