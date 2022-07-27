bbe5652d59f2e65743fd91771298b313b66ebc2c
강민석 <bluekms@shiftup.co.kr>
2022-07-27 19:20:37 +0900

    불필욯나 로그 지우고 localhost 로 수정

--- Libraries/AuthLibrary/Extensions/RedisCacheExtension.cs
+++ Libraries/AuthLibrary/Extensions/RedisCacheExtension.cs
@@ -7,11 +7,7 @@ public static class RedisCacheExtension
 {
     public static void UseRedisCache(this IServiceCollection services, string? connectionString)
     {
-        Console.WriteLine("Start");
         Console.WriteLine($"kkk : {connectionString ?? "null"}");
-        Console.WriteLine($"appsettings.Docker.json : {File.Exists("appsettings.Docker.json")}");
-        Console.WriteLine(File.ReadAllText("appsettings.Docker.json"));
-        Console.WriteLine("End");
 
         var redisConnection = ConnectionMultiplexer.Connect(connectionString);
 
