677836e3c6bd4ea7b8034dd0d73b30004127b6c0
Minseok.Kang <bluekms21@naver.com>
2022-08-17 17:54:35 +0900

    Use Grpc Extension (#40)

--- Servers/WorldServer/Program.cs
+++ Servers/WorldServer/Program.cs
@@ -14,6 +14,7 @@ using WorldServer.Extensions;
 using WorldServer.Services;
 
 // https://docs.microsoft.com/ko-kr/aspnet/core/grpc/?view=aspnetcore-6.0
+// how to use both grpc and controller in asp dotnet core
 //
 var builder = WebApplication.CreateBuilder(args);
 builder.Host.UseLogger();
@@ -21,9 +22,9 @@ builder.Host.UseLogger();
 builder.Services.UseMySql<AuthDbContext>(builder.Configuration.GetConnectionString("AuthDb"));
 builder.Services.UseMySql<WorldDbContext>(builder.Configuration.GetConnectionString("WorldDb"));
 builder.Services.UseRedisCache(builder.Configuration.GetConnectionString("RedisCache"));
+
 builder.Services.UseMapster();
 builder.Services.UseSessionAuthentication();
-builder.Services.UseCredentialAuthentication();
 builder.Services.UsePermissionAuthorization();
 
 builder.Services.UseHandlers(new GenericDerivedTypeSelector(Assembly.GetExecutingAssembly()));
@@ -35,20 +36,18 @@ builder.Services.UseServerRegistry(builder.Configuration.GetSection(ServerRegist
 builder.Services.AddScoped<ITimeService, ScopedTimeService>();
 
 builder.Services.UseControllers();
-
-// Additional configuration is required to successfully run gRPC on macOS.
-// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
-//
-
-// Add services to the container.
-builder.Services.AddGrpc();
+builder.Services.UseGrpc();
 
 var app = builder.Build();
+app.UseRouting();
+app.UseAuthentication();
 app.UseAuthorization();
+app.UseEndpoints(endpoints =>
+{
+    // Configure the HTTP request pipeline.
+    endpoints.MapGrpcService<FooService>();
+    endpoints.MapGrpcService<GreeterService>();
+});
+
 app.MapControllers();
-
-// Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
-app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
-
 app.Run();
