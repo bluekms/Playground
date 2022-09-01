4076215a71e1fc3a6236815f1d775cdb24f79369
강민석 <bluekms@shiftup.co.kr>
2022-09-01 19:17:07 +0900

    월드서버에서 grpc 제거

--- Servers/WorldServer/Program.cs
+++ Servers/WorldServer/Program.cs
@@ -11,20 +11,16 @@ using CommonLibrary.Handlers.Decorators;
 using CommonLibrary.Models;
 using WorldServer;
 using WorldServer.Extensions;
-using WorldServer.Services;
 
-// https://docs.microsoft.com/ko-kr/aspnet/core/grpc/?view=aspnetcore-6.0
-// how to use both grpc and controller in asp dotnet core
-//
 var builder = WebApplication.CreateBuilder(args);
 builder.Host.UseLogger();
 
 builder.Services.UseMySql<AuthDbContext>(builder.Configuration.GetConnectionString("AuthDb"));
 builder.Services.UseMySql<WorldDbContext>(builder.Configuration.GetConnectionString("WorldDb"));
 builder.Services.UseRedisCache(builder.Configuration.GetConnectionString("RedisCache"));
-
 builder.Services.UseMapster();
 builder.Services.UseSessionAuthentication();
+builder.Services.UseCredentialAuthentication();
 builder.Services.UsePermissionAuthorization();
 
 builder.Services.UseHandlers(new GenericDerivedTypeSelector(Assembly.GetExecutingAssembly()));
@@ -36,18 +32,10 @@ builder.Services.UseServerRegistry(builder.Configuration.GetSection(ServerRegist
 builder.Services.AddScoped<ITimeService, ScopedTimeService>();
 
 builder.Services.UseControllers();
-builder.Services.UseGrpc();
 
+// Configure the HTTP request pipeline.
+//
 var app = builder.Build();
-app.UseRouting();
-app.UseAuthentication();
 app.UseAuthorization();
-app.UseEndpoints(endpoints =>
-{
-    // Configure the HTTP request pipeline.
-    endpoints.MapGrpcService<FooService>();
-    endpoints.MapGrpcService<GreeterService>();
-});
-
 app.MapControllers();
 app.Run();
