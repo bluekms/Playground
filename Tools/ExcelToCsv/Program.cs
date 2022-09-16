b006b891dc9045bfe064e7e1f19ece9779c03b8b
Minseok.Kang <bluekms21@naver.com>
2022-09-16 23:10:53 +0900

    Excel To Csv 개발 및 테스트 프로젝트 추가 (#61)

---
+++ Tools/ExcelToCsv/Program.cs
@@ -0,0 +1,27 @@
+﻿using CommandLine;
+using ExcelToCsv;
+
+var args2 = new List<string>();
+args2.Add("-f");
+args2.Add(@"D:\Workspace\gitProject\Playground\StaticData\SampleStaticData.xlsx");
+args2.Add("-o");
+args2.Add(@"D:\Workspace\gitProject\Playground\StaticData\Output");
+
+Parser.Default.ParseArguments<ProgramOptions>(args2)
+    .WithParsed(RunOptions)
+    .WithNotParsed(HandleParseError);
+
+static void RunOptions(ProgramOptions options)
+{
+    var processor = new ExcelConverter();
+    processor.ReadFileStream(options.FileName);
+}
+
+static void HandleParseError(IEnumerable<Error> errors)
+{
+    Console.WriteLine($"Errors {errors.Count()}");
+    foreach (var error in errors)
+    {
+        Console.WriteLine(error.ToString());
+    }
+}
