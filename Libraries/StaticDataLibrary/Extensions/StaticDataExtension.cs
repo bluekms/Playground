ad715bbb263e123dfc416e04c5e60d56976b870e
Minseok.Kang <bluekms21@naver.com>
2022-10-11 16:49:52 +0900

    WorldServer에서 StaticData를 Sqlite에서 가져오는 데모 작성 (#65)

---
+++ Libraries/StaticDataLibrary/Extensions/StaticDataExtension.cs
@@ -0,0 +1,44 @@
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using StaticDataLibrary.DevRecords;
+using StaticDataLibrary.RecordLibrary;
+
+namespace StaticDataLibrary.Extensions;
+
+public static class StaticDataExtension
+{
+    private static string StaticDataPath = "StaticData";
+    private const string Extension = ".csv";
+    
+    public static async void UseStaticData(this IServiceCollection services)
+    {
+        services.AddEntityFrameworkSqlite().AddDbContext<StaticDataContext>();
+        
+        await using var serviceProvider = services.BuildServiceProvider();
+        var context = serviceProvider.GetRequiredService<StaticDataContext>();
+        await context.Database.EnsureDeletedAsync();
+        await context.Database.EnsureCreatedAsync();
+        
+        await using SqliteConnection connection = new SqliteConnection(context.Database.GetConnectionString());
+        await connection.OpenAsync();
+        await using var transaction = await connection.BeginTransactionAsync() as SqliteTransaction;
+        
+        var path = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+        var recordInfoList = RecordFinder.Find<StaticDataContext>();
+        foreach (var recordInfo in recordInfoList)
+        {
+            var fileName = Path.Combine(path, StaticDataPath, $"{recordInfo.SheetName}{Extension}");
+            if (!File.Exists(fileName))
+            {
+                throw new FileNotFoundException(fileName);
+            }
+
+            var dataList = await RecordParser.GetDataList(recordInfo, fileName);
+            
+            await RecordDataInserter.InsertAsync(recordInfo.DbSetName, recordInfo.RecordType, dataList, connection, transaction!);
+        }
+
+        await transaction!.CommitAsync();
+    }
+}
---
+++ Libraries/StaticDataLibrary/StaticDataContext.cs
@@ -0,0 +1,32 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace StaticDataLibrary.DevRecords;
+
+public class StaticDataContext : DbContext
+{
+    private const string DbFileName = "StaticData.db";
+
+    public StaticDataContext()
+    {
+        var folder = Environment.SpecialFolder.LocalApplicationData;
+        var path = Environment.GetFolderPath(folder);
+        DbPath = Path.Join(path, DbFileName);
+    }
+
+    public string DbPath { get; } = null!;
+
+    public DbSet<TargetTestRecord> TargetTestTable { get; set; } = null!;
+
+    public DbSet<NameTestRecord> NameTestTable { get; set; } = null!;
+
+    public DbSet<ArrayTestRecord> ArrayTestTable { get; set; } = null!;
+
+    public DbSet<ClassListTestRecord> ClassListTestTable { get; set; } = null!;
+
+    public DbSet<ComplexTestRecord> ComplexTestTable { get; set; } = null!;
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        options.UseSqlite($"Data Source={DbPath}");
+    }
+}
