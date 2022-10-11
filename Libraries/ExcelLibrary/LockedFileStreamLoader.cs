f1f204f4bc8768247b11c02ee862d683c41f92f3
Minseok.Kang <bluekms21@naver.com>
2022-10-11 16:56:22 +0900

    ExcelLibrary (#64)

---
+++ Libraries/ExcelLibrary/LockedFileStreamLoader.cs
@@ -0,0 +1,54 @@
+using System.Diagnostics;
+
+namespace ExcelLibrary;
+
+public sealed class LockedFileStreamLoader : IDisposable
+{
+    public string? TempFileName { get; }
+
+    public LockedFileStreamLoader(string fileName)
+    {
+        try
+        {
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+            Stream = File.Open(fileName, FileMode.Open, FileAccess.Read);
+        }
+        catch (IOException)
+        {
+            TempFileName = Path.GetTempFileName();
+            ForceCopyAsync(fileName, TempFileName);
+            Stream = File.Open(TempFileName, FileMode.Open, FileAccess.Read);
+        }
+    }
+    
+    public Stream Stream { get; }
+
+    public bool IsTemp => !string.IsNullOrEmpty(TempFileName);
+
+    public void Dispose()
+    {
+        Stream.Dispose();
+        if (!string.IsNullOrEmpty(TempFileName))
+        {
+            if (File.Exists(TempFileName))
+            {
+                File.Delete(TempFileName);
+            }
+        }
+    }
+
+    private static void ForceCopyAsync(string src, string dst)
+    {
+        var command = $"COPY /B /Y {src} {dst}";
+        var process = new Process();
+        process.StartInfo = new ProcessStartInfo
+        {
+            WindowStyle = ProcessWindowStyle.Hidden,
+            FileName = "cmd.exe",
+            Arguments = $"/C {command}",
+        };
+        process.EnableRaisingEvents = true;
+        process.Start();
+        process.WaitForExit();
+    }
+}
---
+++ Libraries/ExcelLibrary/README.md
@@ -0,0 +1,3 @@
+# Excel Library
+
+Server\World\Sqlite를 작업하다가 개발된 것인데 나중에 쓸것같아서 일단 저장해 둔다
