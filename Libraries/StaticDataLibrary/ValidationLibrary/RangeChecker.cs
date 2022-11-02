0fbe2000914bda491159c562d61dc0a714ef60e8
Minseok.Kang <bluekms21@naver.com>
2022-11-02 17:59:45 +0900

    대리자를 사용하여 코드 중복 제거 (#75)

--- Libraries/StaticDataLibrary/ValidationLibrary/RangeChecker.cs
+++ Libraries/StaticDataLibrary/ValidationLibrary/RangeChecker.cs
@@ -1,66 +1,40 @@
 using System.ComponentModel.DataAnnotations;
+using System.Text;
 using Microsoft.EntityFrameworkCore;
-using StaticDataLibrary.RecordLibrary;
 
 namespace StaticDataLibrary.ValidationLibrary;
 
 public static class RangeChecker
 {
-    public static async Task CheckAsync<T>(string staticDataPath) where T : DbContext
+    public static async Task CheckAsync<T>(string staticDataPath, StringBuilder? errors = null) where T : DbContext
     {
-        var tableInfoList = TableFinder.Find<T>();
-        foreach (var tableInfo in tableInfoList)
-        {
-            var fileName = Path.Combine(
-                Directory.GetCurrentDirectory(),
-                staticDataPath,
-                $"{tableInfo.SheetName}.csv");
-            
-            var properties = OrderedPropertySelector.GetList(tableInfo.RecordType);
-            
-            var dataList = await RecordParser.GetDataListAsync(tableInfo, fileName);
-            foreach (var data in dataList)
+        await ValidationAttributeChecker<T, RangeAttribute>.CheckAsync(
+            staticDataPath,
+            (attribute, value, location) =>
             {
-                foreach (var propertyInfo in properties)
+                var message = string.Empty;
+                
+                if (value.GetType() == typeof(DateTime))
                 {
-                    var attribute = PropertyAttributeFinder.Find<RangeAttribute>(tableInfo, propertyInfo.Name);
-                    if (attribute == null)
-                    {
-                        continue;
-                    }
+                    var val = (DateTime)value;
+                    var min = (DateTime)attribute.Minimum;
+                    var max = (DateTime)attribute.Maximum;
 
-                    var value = data.GetType()
-                        .GetProperty(propertyInfo.Name)!
-                        .GetValue(data, null) ?? null;
-
-                    if (value == null)
-                    {
-                        if (propertyInfo.IsNullable())
-                        {
-                            continue;    
-                        }
-                        else
-                        {
-                            throw new ArgumentNullException($"Nullable Error. {tableInfo.RecordType.Name}.{propertyInfo.Name} 반드시 값이 있어야 합니다.");
-                        }
-                    }
-                    
-                    if (!attribute.IsValid(value))
-                    {
-                        if (value.GetType() == typeof(DateTime))
-                        {
-                            var val = (DateTime)value;
-                            var min = (DateTime)attribute.Minimum;
-                            var max = (DateTime)attribute.Maximum;
-                            throw new ValidationException($"Range Error. {tableInfo.RecordType.Name}.{propertyInfo.Name}({val.ToString("s")}) 반드시 {min.ToString("s")} ~ {max.ToString("s")} 사이여야 합니다.");    
-                        }
-                        else
-                        {
-                            throw new ValidationException($"Range Error. {tableInfo.RecordType.Name}.{propertyInfo.Name}({value}) 반드시 {attribute.Minimum} ~ {attribute.Maximum} 사이여야 합니다.");
-                        }
-                    }
-                } // for propertyInfo
-            } // for data
-        } // for table
+                    message = $"Range Error. {location}({val.ToString("s")}) 반드시 {min.ToString("s")} ~ {max.ToString("s")} 사이여야 합니다.";
+                }
+                else
+                {
+                    message = $"Range Error. {location}({value}) 반드시 {attribute.Minimum} ~ {attribute.Maximum} 사이여야 합니다.";
+                }
+                
+                if (errors is null)
+                {
+                    throw new ValidationException(message);
+                }
+                else
+                {
+                    errors.AppendLine(message);
+                }
+            });
     }
 }
--- Libraries/StaticDataLibrary/ValidationLibrary/RegexChecker.cs
+++ Libraries/StaticDataLibrary/ValidationLibrary/RegexChecker.cs
@@ -1,56 +1,26 @@
 using System.ComponentModel.DataAnnotations;
+using System.Text;
 using Microsoft.EntityFrameworkCore;
-using StaticDataLibrary.RecordLibrary;
 
 namespace StaticDataLibrary.ValidationLibrary;
 
 public static class RegexChecker
 {
-    public static async Task CheckAsync<T>(string staticDataPath) where T : DbContext
+    public static async Task CheckAsync<T>(string staticDataPath, StringBuilder? errors = null) where T : DbContext
     {
-        var tableInfoList = TableFinder.Find<T>();
-        foreach (var tableInfo in tableInfoList)
-        {
-            var fileName = Path.Combine(
-                Directory.GetCurrentDirectory(),
-                staticDataPath,
-                $"{tableInfo.SheetName}.csv");
-            
-            var properties = OrderedPropertySelector.GetList(tableInfo.RecordType);
-            
-            var dataList = await RecordParser.GetDataListAsync(tableInfo, fileName);
-            foreach (var data in dataList)
+        await ValidationAttributeChecker<T, RegularExpressionAttribute>.CheckAsync(
+            staticDataPath,
+            (_, value, location) =>
             {
-                foreach (var propertyInfo in properties)
+                var message = $"Regex Error. {location}({value}) 패턴이 맞지 않습니다.";
+                if (errors is null)
                 {
-                    var attribute = PropertyAttributeFinder.Find<RegularExpressionAttribute>(tableInfo, propertyInfo.Name);
-                    if (attribute == null)
-                    {
-                        continue;
-                    }
-
-                    var value = data.GetType()
-                        .GetProperty(propertyInfo.Name)!
-                        .GetValue(data, null) ?? null;
-
-                    if (value == null)
-                    {
-                        if (propertyInfo.IsNullable())
-                        {
-                            continue;    
-                        }
-                        else
-                        {
-                            throw new ArgumentNullException($"Nullable Error. {tableInfo.RecordType.Name}.{propertyInfo.Name} 반드시 값이 있어야 합니다.");
-                        }
-                    }
-                    
-                    if (!attribute.IsValid(value))
-                    {
-                        throw new ValidationException($"Regex Error. {tableInfo.RecordType.Name}.{propertyInfo.Name}({value}) 패턴이 맞지 않습니다.");
-                    }
-                } // for propertyInfo
-            } // for data
-        } // for table
+                    throw new ValidationException(message);
+                }
+                else
+                {
+                    errors.AppendLine($"Regex Error. {location}({value}) 패턴이 맞지 않습니다.");
+                }
+            });
     }
 }
