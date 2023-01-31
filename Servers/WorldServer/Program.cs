268830589775ea070151a04dcf5fb089d6c134a0
강민석 <bluekms@shiftup.co.kr>
2023-01-31 17:15:16 +0900

    remove

--- Servers/WorldServer/Program.cs
+++ Servers/WorldServer/Program.cs
@@ -9,7 +9,8 @@ using CommonLibrary.Extensions;
 using CommonLibrary.Handlers;
 using CommonLibrary.Handlers.Decorators;
 using CommonLibrary.Models;
-using StaticDataLibrary.Extensions;
+
+// using StaticDataLibrary.Extensions;
 using WorldServer;
 using WorldServer.Extensions;
 
@@ -18,7 +19,8 @@ builder.Host.UseLogger();
 builder.Services.UseNginx();
 builder.Services.UseMySql<AuthDbContext>(builder.Configuration.GetConnectionString("AuthDb"));
 builder.Services.UseMySql<WorldDbContext>(builder.Configuration.GetConnectionString("WorldDb"));
-builder.Services.UseStaticData("StaticData");
+
+// builder.Services.UseStaticData("StaticData");
 builder.Services.UseRedisCache(builder.Configuration.GetConnectionString("RedisCache")!);
 builder.Services.UseMapster();
 builder.Services.UseSessionAuthentication();
