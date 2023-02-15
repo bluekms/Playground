5dac39396000b1a970a67b00cc513ea855a687bd
강민석 <bluekms@shiftup.co.kr>
2023-02-15 18:27:52 +0900

    ApplySession

--- Servers/AuthServer/Controllers/FooController.cs
+++ Servers/AuthServer/Controllers/FooController.cs
@@ -7,16 +7,13 @@ namespace AuthServer.Controllers;
 [ApiController]
 public sealed class FooController : ControllerBase
 {
-    public FooController()
-    {
-    }
-
     [HttpPost]
     [Route("Auth/Foo")]
     [Authorize(AuthenticationSchemes = OpenAuthenticationSchemeOptions.Name)]
     public ActionResult<string> Foo([FromBody] Arguments args, CancellationToken cancellationToken)
     {
-        return $"{args.Data}: Ok";
+        // TODO Open일때 Session을 인자로 받지 못하도록 유닛테스트 걸자
+        return $"{args.Data}: Ok.";
     }
 
     public sealed record Arguments(string Data);
