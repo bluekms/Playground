cfc49eb1be4b7df75e2bc124751f65132b8375eb
강민석 <bluekms@shiftup.co.kr>
2023-02-15 18:51:27 +0900

    유닛테스트 수정

--- Servers/AuthServer/Controllers/FooController.cs
+++ Servers/AuthServer/Controllers/FooController.cs
@@ -13,7 +13,7 @@ public sealed class FooController : ControllerBase
     public ActionResult<string> Foo([FromBody] Arguments args, CancellationToken cancellationToken)
     {
         // TODO Open일때 Session을 인자로 받지 못하도록 유닛테스트 걸자
-        return $"{args.Data}: Ok.";
+        return $"{args.Data}: Ok";
     }
 
     public sealed record Arguments(string Data);
