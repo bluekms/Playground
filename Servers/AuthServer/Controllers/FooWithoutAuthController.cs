9956677a6ada01a206f5e40d6a65d5233b7f1e87
강민석 <bluekms21@naver.com>
2023-11-13 16:46:43 +0900

    Foo2에서 Exception 을 발생하도록 수정 (#142)

--- Servers/AuthServer/Controllers/FooWithoutAuthController.cs
+++ Servers/AuthServer/Controllers/FooWithoutAuthController.cs
@@ -10,7 +10,6 @@ public class FooWithoutAuthController : ControllerBase
         [FromBody] ReqFoo request,
         CancellationToken cancellationToken)
     {
-        // TODO Open일때 Session을 인자로 받지 못하도록 유닛테스트 걸자
-        return $"{request.Data}: Ok.";
+        throw new Exception($"{request.Data}");
     }
 }
