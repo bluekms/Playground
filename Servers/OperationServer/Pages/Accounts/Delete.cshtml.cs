cd6712bf471c72cb99e47d187312e4d061d160c4
강민석 <bluekms@shiftup.co.kr>
2023-12-06 19:41:43 +0900

    사용하는 Pages

---
+++ Servers/OperationServer/Pages/Accounts/Delete.cshtml.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using CommonLibrary.Handlers;
+using OperationServer.Handlers.Account;
+using OperationServer.Pages.Accounts.ViewModels;
+
+namespace OperationServer.Pages.Accounts
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly IQueryHandler<GetAccountQuery, AccountVM> getAccount;
+        private readonly ICommandHandler<DeleteAccountCommand> deleteAccount;
+
+        public DeleteModel(
+            IQueryHandler<GetAccountQuery, AccountVM> getAccount,
+            ICommandHandler<DeleteAccountCommand> deleteAccount)
+        {
+            this.getAccount = getAccount;
+            this.deleteAccount = deleteAccount;
+        }
+
+        [BindProperty]
+      public AccountVM Account { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(string accountId)
+        {
+            Account = await getAccount.QueryAsync(new(accountId), CancellationToken.None);
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(string accountId)
+        {
+            await deleteAccount.ExecuteAsync(new(accountId));
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ Servers/OperationServer/Pages/Accounts/EditRole.cshtml.cs
@@ -0,0 +1,54 @@
+using CommonLibrary.Handlers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using OperationServer.Handlers.Account;
+using OperationServer.Pages.Accounts.ViewModels;
+
+namespace OperationServer.Pages.Accounts;
+
+public class EditRoleModel : PageModel
+{
+    private readonly IQueryHandler<GetAccountQuery, AccountUpdateRoleVM> getAccount;
+    private readonly ICommandHandler<UpdateAccountRoleCommand> updateAccountRole;
+
+    public EditRoleModel(
+        IQueryHandler<GetAccountQuery, AccountUpdateRoleVM> getAccount,
+        ICommandHandler<UpdateAccountRoleCommand> updateAccountRole)
+    {
+        this.getAccount = getAccount;
+        this.updateAccountRole = updateAccountRole;
+
+        RoleList = Enum.GetValues(typeof(ResSignUp.Types.AccountRoles))
+            .Cast<ResSignUp.Types.AccountRoles>()
+            .Select(role => new SelectListItem
+            {
+                Value = role.ToString(),
+                Text = role.ToString(),
+            }).ToList();
+    }
+
+    [BindProperty]
+    public AccountUpdateRoleVM Account { get; set; } = default!;
+
+    public List<SelectListItem> RoleList { get; set; }
+
+    public async Task<IActionResult> OnGetAsync(string accountId)
+    {
+        Account = await getAccount.QueryAsync(new(accountId), CancellationToken.None);
+
+        return Page();
+    }
+
+    public async Task<IActionResult> OnPostAsync()
+    {
+        if (!ModelState.IsValid)
+        {
+            return Page();
+        }
+
+        await updateAccountRole.ExecuteAsync(new(Account.AccountId, Account.Role));
+
+        return RedirectToPage("./Index");
+    }
+}
