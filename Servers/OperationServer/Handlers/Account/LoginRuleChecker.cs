be51ba6f2c910cc88387162028f5500b3369a6e0
강민석 <bluekms@shiftup.co.kr>
2024-01-12 17:41:59 +0900

    작업중

--- Servers/OperationServer/Handlers/Account/LoginRuleChecker.cs
+++ Servers/OperationServer/Handlers/Account/LoginRuleChecker.cs
@@ -1,4 +1,7 @@
+using AuthDb;
 using CommonLibrary.Handlers;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 namespace OperationServer.Handlers.Account;
 
@@ -6,8 +9,25 @@ public sealed record LoginRule(string AccountId, string Password, string Salt) :
 
 public class LoginRuleChecker : IRuleChecker<LoginRule>
 {
-    public Task CheckAsync(LoginRule rule, CancellationToken cancellationToken)
-    {
+    private ReadOnlyAuthDbContext dbContext;
 
+    public LoginRuleChecker(ReadOnlyAuthDbContext dbContext)
+    {
+        this.dbContext = dbContext;
+    }
+
+    public async Task CheckAsync(LoginRule rule, CancellationToken cancellationToken)
+    {
+        var account = await dbContext.Accounts
+            .Where(row => row.AccountId == rule.AccountId)
+            .SingleAsync(cancellationToken);
+
+        var passwordHasher = new PasswordHasher<AuthDb.Account>();
+        var result = passwordHasher.VerifyHashedPassword(account, account.Password, rule.Salt + rule.Password);
+
+        if (result is not (PasswordVerificationResult.Success or PasswordVerificationResult.SuccessRehashNeeded))
+        {
+            throw new ArgumentException(rule.Password);
+        }
     }
 }
