83660c4683b8dba315657204df497cc728cb8509
강민석 <bluekms@shiftup.co.kr>
2024-01-16 19:26:42 +0900

    경고 메시지 BPG 에 대한 주석 추가

--- Libraries/Analyzers/DiagnosticIds.cs
+++ Libraries/Analyzers/DiagnosticIds.cs
@@ -2,6 +2,7 @@ namespace Analyzers;
 
 public static class DiagnosticIds
 {
+    // BPG : Bluekms's PlayGround
     public const string ReadOnlyDbContext = "BPG0001";
     public const string SealedHandler = "BPG0002";
 }
