e4674ce523d284af96b338ff0baba146968676b0
강민석 <bluekms@shiftup.co.kr>
2024-01-16 20:08:56 +0900

    기존 마이그래이션들 초기화

---
+++ Libraries/AuthDb/Migrations/20240116110754_InitCreate.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace AuthDb.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "AuthDb");
+
+            migrationBuilder.CreateTable(
+                name: "Foos",
+                schema: "AuthDb",
+                columns: table => new
+                {
+                    Seq = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    AccountId = table.Column<string>(type: "text", nullable: false),
+                    Command = table.Column<string>(type: "text", nullable: false),
+                    Value = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Foos", x => x.Seq);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Maintenances",
+                schema: "AuthDb",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Start = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    End = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Reason = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Maintenances", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Passwords",
+                schema: "AuthDb",
+                columns: table => new
+                {
+                    AccountId = table.Column<string>(type: "text", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    AccountPassword = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Passwords", x => x.AccountId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ServerRoles",
+                schema: "AuthDb",
+                columns: table => new
+                {
+                    Token = table.Column<string>(type: "text", nullable: false),
+                    Role = table.Column<int>(type: "integer", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ServerRoles", x => x.Token);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Servers",
+                schema: "AuthDb",
+                columns: table => new
+                {
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Role = table.Column<int>(type: "integer", nullable: false),
+                    Address = table.Column<string>(type: "text", nullable: false),
+                    ExpireAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Servers", x => x.Name);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Accounts",
+                schema: "AuthDb",
+                columns: table => new
+                {
+                    AccountId = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Token = table.Column<string>(type: "text", nullable: false),
+                    Role = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Accounts", x => x.AccountId);
+                    table.ForeignKey(
+                        name: "FK_Accounts_Passwords_AccountId",
+                        column: x => x.AccountId,
+                        principalSchema: "AuthDb",
+                        principalTable: "Passwords",
+                        principalColumn: "AccountId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Accounts",
+                schema: "AuthDb");
+
+            migrationBuilder.DropTable(
+                name: "Foos",
+                schema: "AuthDb");
+
+            migrationBuilder.DropTable(
+                name: "Maintenances",
+                schema: "AuthDb");
+
+            migrationBuilder.DropTable(
+                name: "ServerRoles",
+                schema: "AuthDb");
+
+            migrationBuilder.DropTable(
+                name: "Servers",
+                schema: "AuthDb");
+
+            migrationBuilder.DropTable(
+                name: "Passwords",
+                schema: "AuthDb");
+        }
+    }
+}
